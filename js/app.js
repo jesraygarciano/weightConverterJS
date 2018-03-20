030e9c82547ae3d96f0daebdb1d13f0368919327
Jesray Garciano <jesray.garciano@gmail.com>
2018-03-20 13:26:23 +0800

    Adding CSS to Grams, kilo grams and Ounces

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
+document.getElementById('output').style.visibility = 'hidden';
 document.getElementById('lbsInput').addEventListener('input', function(e){
+
+    document.getElementById('output').style.visibility = 'visible';    
     // console.log(123);
     let pounds = e.target.value;
     // console.log(pounds);
 
     document.getElementById('gramsOutput').innerHTML = pounds/0.0022046;
-    document.getElementById('kgOutput').innerHTML = pounds/0.0022046;
-    document.getElementById('ozOutput').innerHTML = pounds/0.0022046;
+    document.getElementById('kgOutput').innerHTML = pounds/2.4046;
+    document.getElementById('ozOutput').innerHTML = pounds*16;
 
-})
+});
